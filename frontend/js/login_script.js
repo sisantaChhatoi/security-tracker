cc9c2ee7bfe551324d4d8aad3cee668165345718
prana-w <pramadhu.kumar@gmail.com>
2025-02-14 17:33:36 +0530

    Add dynamic typing in login.html

--- frontend/js/login_script.js
+++ frontend/js/login_script.js
@@ -4,7 +4,7 @@ const phoneNumElem = document.getElementById("mobNumber");
 const pwdElem = document.getElementById("pwd");
 
 //! Include mail address in the logic of the code
-const mailElement = document.getElementById ('emailAddress')
+const mailElement = document.getElementById("emailAddress");
 const loginBtn = document.getElementById("logInBtn");
 
 //check if guard is registered or not
@@ -25,8 +25,6 @@ async function isGuardRegistered(phoneNum) {
 
 //Does the login (or rejects if not registered)
 loginBtn.addEventListener("click", (e) => {
-
-
   //Checks if the input field is empty
   if (!phoneNumElem.value.trim()) {
     return;
@@ -47,3 +45,77 @@ loginBtn.addEventListener("click", (e) => {
     }
   });
 });
+let arr; //this is the array of strings
+
+const timeDelay = 120;
+const factor = 5;
+
+const txtArea = document.querySelector(".typed-text");
+
+function deleteLetters(value) {
+  let index = value.length;
+
+  const endId = setInterval(() => {
+    let newTxt = value.substr(0, index);
+    index--;
+
+    txtArea.innerHTML = newTxt;
+
+    if (index == -1) {
+      clearInterval(endId);
+
+      randomIdgenerator();
+    }
+  }, timeDelay);
+}
+
+function addLetters(i) {
+  const value = arr[i];
+  console.log(arr);
+  if (arr.length == 0) {
+    console.log("END");
+
+    setTimeout(newLetters, timeDelay * factor);
+
+    return;
+  }
+
+  arr.splice(i, 1);
+
+  let index = 0;
+
+  const startId = setInterval(() => {
+    let newTxt = value.substr(0, index + 1);
+    index++;
+
+    txtArea.innerHTML = newTxt;
+
+    if (index == value.length) {
+      clearInterval(startId);
+
+      setTimeout(() => {
+        deleteLetters(value);
+      }, timeDelay * factor);
+    }
+  }, timeDelay);
+}
+
+function randomIdgenerator() {
+  const i = Math.floor(Math.random() * arr.length);
+
+  setTimeout(() => {
+    addLetters(i);
+  }, timeDelay * factor);
+}
+
+async function newLetters() {
+  let randomTextCollection = await fetch("extraTxt.txt");
+
+  randomTextCollection = await randomTextCollection.text();
+
+  arr = await randomTextCollection.split("\n");
+
+  randomIdgenerator();
+}
+
+newLetters();
